5efb5bb3b1564a62af27e55b3af506955dcea066
App Academy Student <student@appacademy.io>
2013-08-07 10:33:32 -0700

    Initial commit.

---
+++ Router/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Router::Application.config.secret_token = '2af9f58bf5ee43b965b0ca965903db88b7445a929d1a4d076d6fe96d841b405c082987dec75eaeb7ac3f6153d3e1d417384de2ef7fab4cfb9f01c57ae6af24fe'
